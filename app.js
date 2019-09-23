57b86c465dbdb528c23939dddeba6a08a7592dec
lhikarul <711hikaru@gmail.com>
2019-09-23 17:14:28 +0800

    section 6 Natours API completed

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const morgan  = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+if (process.env.NODE === 'development') {
+    app.use(morgan('dev'));
+}
+
 const app = express();
 
 app.use(express.json());
@@ -13,5 +17,12 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(3000);
+app.listen(3000)
+
+// const port = process.env.PORT || 2000;
+
+// app.listen(port, () => {
+//     console.log(`App running on port ${port}...`);
+// });
+
 // module.exports = app;
--- sever.js
+++ sever.js
@@ -1,3 +1,10 @@
-const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'})
 
-app.listen(3000);
+// const {app} = require('./app');
+
+const port = process.env.PORT || 2000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
