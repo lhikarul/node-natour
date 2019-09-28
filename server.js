ebf392c9d1124ba8ab866546c6bca12cf2d1e31a
lhikarul <711hikaru@gmail.com>
2019-09-28 16:46:46 +0800

    85. creating documents and testing the model

--- server.js
+++ server.js
@@ -34,6 +34,17 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 497
+})
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('error ', err)
+})
 
 const port = process.env.PORT || 2000;
 
