f0a6a97508c028b16a9770f41e642c049ff8c288
lhikarul <711hikaru@gmail.com>
2019-10-08 10:42:59 +0800

    99. making the api better Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,14 @@
 const Tour = require('../models/tourModel');
 
+exports.alisaTopTours = (req,res,next) => {
+
+    req.query.limit = '5';
+    req.query.sort  = '-ratingAverage,price';
+    req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+
+    next();
+}
+
 exports.getAllTours = async (req,res) => {
     
     try {
