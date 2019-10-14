2e54ec2c3ecadc05f2454b714fe06e3b2d607688
lhikarul <711hikaru@gmail.com>
2019-10-14 15:37:49 +0800

    131. accessing protected routes when auth passed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
 const signToken = id => {
     return jwt.sign({id}, process.env.JWT_SECRET,{
@@ -10,12 +11,14 @@ const signToken = id => {
 
 exports.signup = catchAsync(async (req, res, next) => {
 
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
-    });
+    // const newUser = await User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     passwordConfirm: req.body.passwordConfirm
+    // });
+
+    const newUser = await User.create(req.body);
 
     const token = signToken(newUser._id);
 
@@ -35,18 +38,14 @@ exports.login = catchAsync(async (req, res, next) => {
 
     // 1) Check if email and password exist
     if (!email || !password) {
-        console.log('email or password does not exist')
-        return;
-        // return next(new AppError('Please provide email and password!', 400));
+        return next(new AppError('Please provide email and password!', 400));
     }
 
     // 2) Check if user exists && password is correct
     const user = await User.findOne({ email }).select('+password');
 
     if (!user || !(await user.correctPassword(password, user.password))) {
-        console.log('password is not correct')
-        return;
-        // return next(new AppError('Incorrect email or password', 401));
+        return next(new AppError('Incorrect email or password', 401));
     }
 
     // 3) If everything ok, send token to client
@@ -67,11 +66,34 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
 
     if (!token) {
-        console.log('You are not logged in! Please log in to get access.')
-        // return next(
-        // new AppError('You are not logged in! Please log in to get access.', 401)
-        // );
+        return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+        );
     }
 
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+     // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        );
+    }
+
+     // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again.', 401)
+        );
+    }
+    
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
     next();
 });
