11e1a6930b9882127221e6735089f6ee54df000c
lhikarul <711hikaru@gmail.com>
2019-10-14 17:10:01 +0800

    133. only user admin allows deleting tours.

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,14 +11,14 @@ const signToken = id => {
 
 exports.signup = catchAsync(async (req, res, next) => {
 
-    // const newUser = await User.create({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    //     passwordConfirm: req.body.passwordConfirm
-    // });
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
 
-    const newUser = await User.create(req.body);
+    // const newUser = await User.create(req.body);
 
     const token = signToken(newUser._id);
 
@@ -97,3 +97,17 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+
+        // roles ['admin', 'lead-guide']. role='user'
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError('You do not have permission to perform this action', 403)
+            );
+        }
+
+        next();
+    };
+};
