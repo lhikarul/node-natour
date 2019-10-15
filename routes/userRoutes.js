232d88e62ba4f2707d5022082a7e0515d0144f50
lhikarul <711hikaru@gmail.com>
2019-10-15 10:42:37 +0800

    update password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/singup',authController.signup);
 router.post('/login',authController.login);
 router.post('/forgotPassword',authController.forgotPassword);
 router.patch('/resetPassword/:token',authController.resetPassword);
+router.patch('/updatePassword',authController.protect, authController.updatePassword);
 
 router
     .route('/')
