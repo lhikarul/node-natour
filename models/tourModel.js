2f9661fcf08a712be3959e919466d62076cc1120
lhikarul <711hikaru@gmail.com>
2019-10-17 10:37:04 +0800

    152. populating tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -105,6 +105,15 @@ tourSchema.pre('save', function (next) {
     next();
 })
 
+tourSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    });
+
+    next();
+});
+
 // tourSchema.pre('save', async function(next) {
 //     const guidesPromises = this.guides.map(async id => await User.findById(id));
 //     this.guides = await Promise.all(guidesPromises);
